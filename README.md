bdcea2a180fd1273c19c06d5ba2b21c1020cc296
Josué Daniel Flores <mr.danielfo@gmail.com>
2020-04-11 02:33:37 -0500

    Selector v1.0 terminado

---
+++ README.md
@@ -0,0 +1,3 @@
+# Selector de Pizzas con HTML, CSS Y JS
+## Versión 1.0
+### 10/04/2020
---
+++ js/app.js
@@ -0,0 +1,417 @@
+// variables globales 
+let pizzaSeleccionada = document.getElementById('sabor-pizza');
+let pizzaCantidad = document.getElementById('cantidad-pizza');
+let ctnPizzaSeleccionada = document.querySelector('.pizza-seleccionada');
+let cuadroPago = document.querySelector('.cuadro-pago');
+let precioCantidad = document.querySelector('.precio-cantidad');
+let precioTotal = document.querySelector('.total-cantidad');
+
+/* Ingredientes extra */
+let extraQueso = document.getElementById('extra-queso'),
+    extraPinneapleMango = document.getElementById('pineapple-mango'),
+    costillas = document.getElementById('costillas');
+
+/* paquetes extra */
+let refresco = document.getElementById('refresco'),
+    donas = document.getElementById('donas'),
+    chocolate = document.getElementById('chocolate'),
+    payFresa = document.getElementById('payfresa');
+
+let cantidadRefresco = document.getElementById('cantidad-refresco'),
+     cantidadDonas = document.getElementById('cantidad-donas'),
+     cantidadChocolate = document.getElementById('cantidad-chocolate'),
+     cantidadPay = document.getElementById('cantidad-pay');
+
+/* contenedores ingredientes extra */
+let ctnExtraQueso = document.querySelector('.extra-queso'),
+    ctnExtraPineapple = document.querySelector('.extrapineapple'),
+    ctnExtraCostillas = document.querySelector('.costillas');
+
+/* contenedores paquetes extra */
+let ctnRefresco = document.querySelector('.refresco'),
+    ctnDonas = document.querySelector('.donas'),
+    ctnChocolate = document.querySelector('.chocolate'),
+    ctnPayFresa = document.querySelector('.payfresa');
+/* Contenedores cantidad paquetes extra */
+
+let ctnCantidadRefresco = document.querySelector('.precio-cantidad.paq-extra.paq-refreso');
+let ctnCantidadDonas = document.querySelector('.precio-cantidad.paq-extra.paq-donas');
+let ctnCantidadChocolate = document.querySelector('.precio-cantidad.paq-extra.paq-chocolate');
+let ctnCantidadPay = document.querySelector('.precio-cantidad.paq-extra.paq-pay')
+
+/* Arreglo de ingredientes extra */ 
+let arregloPreciosExtra = [];
+/* Arreglo de paquetes extra */
+let arregloPaquetesExtra = [];
+
+
+/* detectar sabor de pizza */
+pizzaSeleccionada.addEventListener('change', pizzaCantidadSabor)
+
+function pizzaCantidadSabor() {
+     let imagen;
+     imagen = document.createElement('img');
+     if (ctnPizzaSeleccionada.childNodes.length >= 2) {
+          // console.log(ctnPizzaSeleccionada.childNodes)
+          ctnPizzaSeleccionada.removeChild(ctnPizzaSeleccionada.childNodes[1])
+     }
+     if (pizzaSeleccionada.value === 'hawaiana') {
+          imagen.setAttribute('src', 'img/pizzahawaiana.png');
+          imagen.setAttribute('alt', 'pizza-hawaiana')
+     } else if (pizzaSeleccionada.value === 'pepperoni') {
+          imagen.setAttribute('src', 'img/pizzapepperoni.png');
+          imagen.setAttribute('alt', 'pizza-pepperoni')
+     } else if (pizzaSeleccionada.value === 'vegetales') {
+          imagen.setAttribute('src', 'img/pizzaveggie.png');
+          imagen.setAttribute('alt', 'pizza-veggie')
+     } else if (pizzaSeleccionada.value === 'mexicana') {
+          imagen.setAttribute('src', 'img/pizzamexicana.png');
+          imagen.setAttribute('alt', 'pizza-mexicana')
+     } else if (pizzaSeleccionada.value === 'camaron') {
+          imagen.setAttribute('src', 'img/pizzacamaron.png');
+          imagen.setAttribute('alt', 'pizza-camaron')
+     } else if  (pizzaSeleccionada.value === 'serrano') {
+          imagen.setAttribute('src', 'img/pizzaserrano.png');
+          imagen.setAttribute('alt', 'pizza-serrano')
+     }
+     ctnPizzaSeleccionada.appendChild(imagen)
+     setTimeout(() => {
+          cuadroPago.style.opacity = '1';
+     }, 500)
+
+     pizzaCantidad.addEventListener('change', conteoPizzas)
+     extraQueso.addEventListener('change', ingredienteExtraQueso)
+     extraPinneapleMango.addEventListener('change', ingredientePineappleMango)
+     costillas.addEventListener('change', ingredienteCostillas)
+     refresco.addEventListener('change', paqueteRefresco)
+     donas.addEventListener('change', paqueteDonas)
+     chocolate.addEventListener('change', paqueteChocolate)
+     payFresa.addEventListener('change', paquetePay)
+}
+
+/* detectar si se escoge un ingrediente extra */
+
+function ingredienteExtraQueso(e) {
+     let precioExtraQueso;
+     let imagen;
+     imagen = document.createElement('img');
+     if (extraQueso.checked) {
+          precioExtraQueso = parseInt(e.target.parentElement.childNodes[5].innerText.split(' ')[1])
+          arregloPreciosExtra.push(precioExtraQueso);
+          imagen.setAttribute('src', 'img/extra-queso2.png');
+          imagen.setAttribute('alt', 'extra-queso');
+          ctnExtraQueso.appendChild(imagen);
+          calcularPrecioTotal()
+     } else {
+          // console.log(ctnExtraQueso.childNodes)
+          precioExtraQueso = 0;
+          arregloPreciosExtra.splice(0, 1);
+          ctnExtraQueso.removeChild(ctnExtraQueso.childNodes[1])
+          calcularPrecioTotal();
+     }
+}
+
+function ingredientePineappleMango(e) {
+     let precioExtraPineapple;
+     let imagen;
+     imagen = document.createElement('img');
+     if (extraPinneapleMango.checked) {
+          // console.log(e.target.parentElement.childNodes[5])
+           precioExtraPineapple = parseInt(e.target.parentElement.childNodes[5].innerText.split(' ')[1])
+           arregloPreciosExtra.push(precioExtraPineapple);
+           imagen.setAttribute('src', 'img/pineappleextra.png');
+           imagen.setAttribute('alt', 'pineapple');
+           ctnExtraPineapple.appendChild(imagen);
+           calcularPrecioTotal()
+     } else {
+          
+          precioExtraPineapple = 0;
+          arregloPreciosExtra.splice(0, 1);
+          ctnExtraPineapple.removeChild(ctnExtraPineapple.childNodes[1])
+          calcularPrecioTotal();
+     }
+}
+
+function ingredienteCostillas(e) {
+     let precioExtraCostilla;
+     let imagen;
+     imagen = document.createElement('img');
+     if (costillas.checked) {
+          // console.log(e.target.parentElement.childNodes[5])
+           precioExtraCostilla = parseInt(e.target.parentElement.childNodes[5].innerText.split(' ')[1])
+           arregloPreciosExtra.push(precioExtraCostilla);
+           imagen.setAttribute('src', 'img/costillas.png');
+           imagen.setAttribute('alt', 'costillas');
+           ctnExtraCostillas.appendChild(imagen);
+           calcularPrecioTotal()
+     } else {
+          precioExtraCostilla = 0;
+          arregloPreciosExtra.splice(0, 1);
+          ctnExtraCostillas.removeChild(ctnExtraCostillas.childNodes[1])
+          calcularPrecioTotal();
+     }
+}
+
+/* detectar si se escoge un paquete extra */
+
+function paqueteRefresco() {
+     let imagen;
+     imagen = document.createElement('img');
+     if (refresco.checked) {
+          if ( parseInt(cantidadRefresco.value) === 1) {
+                precioExtraRefresco = 40;
+                arregloPaquetesExtra.push(precioExtraRefresco);
+                calcularPrecioTotal()
+          } 
+          cantidadRefresco.addEventListener('change', () => {
+            /* Aquí */ 
+            /* Index of */
+            cantidadExtraRefresco = parseInt(cantidadRefresco.value);
+            if (cantidadExtraRefresco === 1) {
+                precioExtraRefresco = 40;
+               if (arregloPaquetesExtra.length > 1) {
+                    const posicion = (index) => index == precioExtraRefresco + 40;
+                    let refrescoIndex = arregloPaquetesExtra.findIndex(posicion);
+                    arregloPaquetesExtra.splice(refrescoIndex, 1, precioExtraRefresco);
+                     // arregloPaquetesExtra.splice(0, 1, precioExtraRefresco);
+               } else {
+                    arregloPaquetesExtra.splice(0, 1, precioExtraRefresco);
+               }    
+            } else {
+                precioExtraRefresco = 40 * cantidadExtraRefresco;
+                if (arregloPaquetesExtra.length > 1) {
+                     // findIndex()
+                    //  console.log(cantidadExtraRefresco);
+                    //  console.log(precioExtraRefresco);
+                     const posicion = (index) => index == 40 || index == precioExtraRefresco - 40 || index == precioExtraRefresco + 40;
+                     let refrescoIndex = arregloPaquetesExtra.findIndex(posicion);
+                     arregloPaquetesExtra.splice(refrescoIndex, 1, precioExtraRefresco);
+               } else {
+                     arregloPaquetesExtra.splice(0, 1, precioExtraRefresco);
+               }
+            }
+               
+                ctnCantidadRefresco.innerHTML = '$ ' + precioExtraRefresco;
+                calcularPrecioTotal()
+          })
+          imagen.setAttribute('src', 'img/refresco.png');
+          imagen.setAttribute('alt', 'refresco');
+          ctnRefresco.appendChild(imagen);
+     } else {
+
+          precioExtraRefresco = parseInt(ctnCantidadRefresco.innerHTML.split(' ')[1]);
+          if (arregloPaquetesExtra.length > 1) {
+               const precioRemovido = arregloPaquetesExtra.filter( precio => precio !== precioExtraRefresco);
+               arregloPaquetesExtra = precioRemovido;
+          } else {
+               arregloPaquetesExtra.splice(0, 1);
+          }
+          ctnRefresco.removeChild(ctnRefresco.childNodes[3]);
+          cantidadRefresco.value = 1;
+          ctnCantidadRefresco.innerHTML = '$ ' + 40;
+          calcularPrecioTotal()
+     }
+}
+
+function paqueteDonas() {
+     let precioExtraDonas;
+     let imagen;
+     imagen = document.createElement('img');
+     if (donas.checked) {
+          if ( parseInt(cantidadDonas.value) === 1) {
+               precioExtraDonas = 35;
+               arregloPaquetesExtra.push(precioExtraDonas);
+               calcularPrecioTotal()
+         } 
+          cantidadDonas.addEventListener('change', () => {
+               cantidadExtraDonas = parseInt(cantidadDonas.value);
+               if (cantidadExtraDonas === 1) {
+                    precioExtraDonas = 35;
+                    if (arregloPaquetesExtra.length > 1) {
+                         const posicion = (index) => index == precioExtraDonas + 35;
+                         let donasIndex = arregloPaquetesExtra.findIndex(posicion);
+                         arregloPaquetesExtra.splice(donasIndex, 1, precioExtraDonas);
+                          // arregloPaquetesExtra.splice(0, 1, precioExtraRefresco);
+                    } else {
+                         arregloPaquetesExtra.splice(0, 1, precioExtraDonas);
+                    }
+               } else {
+                    precioExtraDonas = 35 * cantidadExtraDonas;
+                    if (arregloPaquetesExtra.length > 1) {
+                         const posicion = (index) => index == 35 || index == precioExtraDonas - 35 || index == precioExtraDonas + 35;
+                         let donasIndex = arregloPaquetesExtra.findIndex(posicion);
+                         arregloPaquetesExtra.splice(donasIndex, 1, precioExtraDonas);
+                   } else {
+                         arregloPaquetesExtra.splice(0, 1, precioExtraDonas);
+                   }
+               }
+                   
+                    ctnCantidadDonas.innerHTML = '$ ' + precioExtraDonas;
+                    calcularPrecioTotal()
+         })
+          imagen.setAttribute('src', 'img/donnut.png');
+          imagen.setAttribute('alt', 'donas');
+          ctnDonas.appendChild(imagen);
+          
+     } else {
+          precioExtraDonas = parseInt(ctnCantidadDonas.innerHTML.split(' ')[1]);
+          if (arregloPaquetesExtra.length > 1) {
+               const precioRemovido = arregloPaquetesExtra.filter( precio => precio !== precioExtraDonas);
+               arregloPaquetesExtra = precioRemovido;
+          } else {
+               arregloPaquetesExtra.splice(0, 1);
+          }
+          ctnDonas.removeChild(ctnDonas.childNodes[3]);
+          cantidadDonas.value = 1;
+          ctnCantidadDonas.innerHTML = '$ ' + 35;
+          calcularPrecioTotal()
+     }
+}
+
+function paqueteChocolate() {
+     let precioExtraChocolate;
+     let imagen;
+     imagen = document.createElement('img');
+     if (chocolate.checked) {
+          if (parseInt(cantidadChocolate.value) === 1) {
+               precioExtraChocolate = 25;
+               arregloPaquetesExtra.push(precioExtraChocolate);
+               calcularPrecioTotal()
+         } 
+          cantidadChocolate.addEventListener('change', () => {
+               /* Aquí */ 
+               cantidadExtraChocolate = parseInt(cantidadChocolate.value);
+               if (cantidadExtraChocolate === 1) {
+                    precioExtraChocolate = 25;
+                    if (arregloPaquetesExtra.length > 1) {
+                         const posicion = (index) => index == precioExtraChocolate + 25;
+                         let chocolateIndex = arregloPaquetesExtra.findIndex(posicion);
+                         arregloPaquetesExtra.splice(chocolateIndex, 1, precioExtraChocolate);
+                    } else {
+                         arregloPaquetesExtra.splice(0, 1, precioExtraChocolate);
+                    }
+               } else {
+                    precioExtraChocolate = 25 * cantidadExtraChocolate;
+                    if (arregloPaquetesExtra.length > 1) {
+                         const posicion = (index) => index == 25 || index == precioExtraChocolate - 25 || index == precioExtraChocolate + 25;
+                         let chocolateIndex = arregloPaquetesExtra.findIndex(posicion);
+                         arregloPaquetesExtra.splice(chocolateIndex, 1, precioExtraChocolate);
+                   } else {
+                         arregloPaquetesExtra.splice(0, 1, precioExtraChocolate);
+                   }
+               }
+               ctnCantidadChocolate.innerHTML = '$ ' + precioExtraChocolate;
+               calcularPrecioTotal()
+               /* Aquí */
+         })
+          imagen.setAttribute('src', 'img/chocolate.png');
+          imagen.setAttribute('alt', 'chocolate');
+          ctnChocolate.appendChild(imagen);
+          
+     } else {
+          precioExtraChocolate = parseInt(ctnCantidadChocolate.innerHTML.split(' ')[1]);
+          if (arregloPaquetesExtra.length > 1) {
+               const precioRemovido = arregloPaquetesExtra.filter( precio => precio !== precioExtraChocolate);
+               arregloPaquetesExtra = precioRemovido;
+          } else {
+               arregloPaquetesExtra.splice(0, 1);
+          }
+          ctnChocolate.removeChild(ctnChocolate.childNodes[3]);
+          cantidadChocolate.value = 1;
+          ctnCantidadChocolate.innerHTML = '$ ' + 25;
+          calcularPrecioTotal()
+     }
+}
+
+function paquetePay() {
+     let precioExtraPay;
+     let imagen;
+     imagen = document.createElement('img');
+     if (payFresa.checked) {
+          if (parseInt(cantidadPay.value) === 1) {
+               precioExtraPay = 30;
+               arregloPaquetesExtra.push(precioExtraPay);
+               calcularPrecioTotal()
+         }
+          cantidadPay.addEventListener('change', () => {
+               cantidadExtraPay = parseInt(cantidadPay.value);
+               /* Aquí */
+               if (cantidadExtraPay === 1) {
+                    precioExtraPay = 30;
+                    if (arregloPaquetesExtra.length > 1) {
+                         const posicion = (index) => index == precioExtraPay + 30;
+                         let payIndex = arregloPaquetesExtra.findIndex(posicion);
+                         arregloPaquetesExtra.splice(payIndex, 1, precioExtraPay);
+                    } else {
+                         arregloPaquetesExtra.splice(0, 1, precioExtraPay);
+                    }
+               } else {
+                    precioExtraPay = 30 * cantidadExtraPay;
+                    if (arregloPaquetesExtra.length > 1) {
+                         const posicion = (index) => index == 30 || index == precioExtraPay - 30 || index == precioExtraPay + 30;
+                         let payIndex = arregloPaquetesExtra.findIndex(posicion);
+                         arregloPaquetesExtra.splice(payIndex, 1, precioExtraPay);
+                   } else {
+                         arregloPaquetesExtra.splice(0, 1, precioExtraPay);
+                   }
+               }
+               /* Aquí */
+               ctnCantidadPay.innerHTML = '$ ' + precioExtraPay;
+               calcularPrecioTotal()
+         })
+          imagen.setAttribute('src', 'img/payfresa.png');
+          imagen.setAttribute('alt', 'payfresa');
+          ctnPayFresa.appendChild(imagen);   
+     } else {
+          precioExtraPay = parseInt(ctnCantidadPay.innerHTML.split(' ')[1]);
+          // console.log(precioExtraPay);
+          if (arregloPaquetesExtra.length > 1) {
+               const precioRemovido = arregloPaquetesExtra.filter( precio => precio !== precioExtraPay);
+               arregloPaquetesExtra = precioRemovido;
+          } else {
+               arregloPaquetesExtra.splice(0, 1);
+          }
+          ctnPayFresa.removeChild(ctnPayFresa.childNodes[3]);
+          cantidadPay.value = 1;
+          ctnCantidadPay.innerHTML = '$ ' + 30;
+          calcularPrecioTotal()
+     }
+}
+
+
+/* functión para contar pizzas */
+
+function conteoPizzas() {
+     let precioUnidad = 220;
+     let cantidadTemporal = parseInt(pizzaCantidad.value)
+     // console.log(precioUnidad, cantidadTemporal)
+     precioCantidad.innerHTML = (precioUnidad * cantidadTemporal);
+     calcularPrecioTotal()
+}
+
+function calcularPrecioTotal() {
+     // console.log(arregloPreciosExtra)
+     if (arregloPreciosExtra.length > 0 ) {
+          let sumador = (total, actual) => total + actual;
+          let ingredientesExtra = arregloPreciosExtra.reduce(sumador);
+          precioTotal.innerHTML = '$' + (parseInt(precioCantidad.innerHTML) + ingredientesExtra); 
+
+          if (arregloPaquetesExtra.length > 0) {
+               // console.log(arregloPaquetesExtra);
+               let sumadorPaquetes = (total, actual) => total + actual;
+               let paquetesExtra = arregloPaquetesExtra.reduce(sumadorPaquetes);
+               // console.log(paquetesExtra);
+               precioTotal.innerHTML = '$' + (parseInt(precioCantidad.innerHTML) + ingredientesExtra + paquetesExtra);
+          }     
+
+     } else if (arregloPaquetesExtra.length > 0) {
+          let sumadorPaquetes = (total, actual) => total + actual;
+          let paquetesExtra = arregloPaquetesExtra.reduce(sumadorPaquetes);
+          precioTotal.innerHTML = '$' + (parseInt(precioCantidad.innerHTML) + paquetesExtra);
+     } else {
+          precioTotal.innerHTML = '$' + parseInt(precioCantidad.innerHTML);
+     }
+}
+
+
+          
